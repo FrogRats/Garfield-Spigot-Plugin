d4903789fd3a6bd05b841bd3626fba54d54b5a6e
Nicholas McCaig <nick.mccaig@hotmail.co.uk>
2020-11-28 23:09:21 +0000

    Moved Lasage recipe to own class

--- src/me/BadutCrew/GarfieldPlugin/Main.java
+++ src/me/BadutCrew/GarfieldPlugin/Main.java
@@ -1,23 +1,18 @@
 package me.BadutCrew.GarfieldPlugin;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.ShapedRecipe;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import me.BadutCrew.GarfieldPlugin.events.EatsLasagne;
 import me.BadutCrew.GarfieldPlugin.events.JoinEvent;
+import me.BadutCrew.GarfieldPlugin.recipe.Lasagne;
 public class Main extends JavaPlugin{
 	
     @Override
     public void onEnable() {
 
     	
-    	Bukkit.addRecipe(getRecipe());
+    	Bukkit.addRecipe(Lasagne.getRecipe());
     	PluginManager pm = this.getServer().getPluginManager();
     	pm.registerEvents(new EatsLasagne(), this);
     	pm.registerEvents(new JoinEvent(), this);
@@ -28,27 +23,4 @@ public class Main extends JavaPlugin{
 
     }
     
-
-	public ShapedRecipe getRecipe() {
-		
-		ItemStack item = new ItemStack(Material.ROTTEN_FLESH);
-		ItemMeta meta = item.getItemMeta();
-
-		meta.setDisplayName(ChatColor.GOLD + "Lasagne");
-		
-		meta.setCustomModelData(1234567);
-		item.setItemMeta(meta);
-		NamespacedKey key = new NamespacedKey(this, "Lasagne");
-		
-		ShapedRecipe recipe = new ShapedRecipe(key, item);
-		
-		recipe.shape("WMW", "EWE", "SSS");
-		
-		recipe.setIngredient('W', Material.WHEAT);
-		recipe.setIngredient('M', Material.MILK_BUCKET);
-		recipe.setIngredient('E', Material.EGG);
-		recipe.setIngredient('S', Material.COOKED_BEEF);
-		
-		return recipe;
-	}
 }
