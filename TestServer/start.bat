35bee99c6c059ef3bcf66e9d339b6e7622d050f4
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2020-11-28 10:52:56 +0000

    Remove test server

--- TestServer/start.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-Java -Xms512M -Xmx512M -jar spigot-1.16.4.jar nogui
-pause
--- TestServer/work/decompile-13da6e65/net/minecraft/server/CryptographyException.java
+++
@@ -1,8 +0,0 @@
-package net.minecraft.server;
-
-public class CryptographyException extends Exception {
-
-    public CryptographyException(Throwable throwable) {
-        super(throwable);
-    }
-}
