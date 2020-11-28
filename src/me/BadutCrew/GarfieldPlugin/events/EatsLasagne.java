5a1dc9240a926efd2ea94e88698aeaae615673ad
Nicholas McCaig <nick.mccaig@hotmail.co.uk>
2020-11-28 15:58:19 +0000

    Update to eats function
    changed so that character spawns whever any player eats

--- src/me/BadutCrew/GarfieldPlugin/events/EatsLasagne.java
+++ src/me/BadutCrew/GarfieldPlugin/events/EatsLasagne.java
@@ -1,20 +1,16 @@
 package me.BadutCrew.GarfieldPlugin.events;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.craftbukkit.v1_16_R3.CraftWorld;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockPlaceEvent;
-
+import org.bukkit.event.player.PlayerItemConsumeEvent;
 import me.BadutCrew.GarfieldPlugin.Mob.Garfield;
 import net.minecraft.server.v1_16_R3.WorldServer;
 
 public class EatsLasagne implements Listener{
 	@EventHandler
-	public void placeblock(BlockPlaceEvent event) {
-		if(!event.getBlock().getType().equals(Material.GOLD_BLOCK))
-			return;
+	public void onConsume(PlayerItemConsumeEvent event) {
 		Garfield gar = new Garfield(event.getPlayer().getLocation());
 		WorldServer world = ((CraftWorld) event.getPlayer().getWorld()).getHandle();
 		world.addEntity(gar);
