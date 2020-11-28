743832cf3720c65b01970f637a84e33e5d7ef797
Hassaan-Naveed <hassaann688@gmail.com>
2020-11-28 23:27:08 +0000

    Updated Garfield.java

--- src/me/BadutCrew/GarfieldPlugin/Mob/Garfield.java
+++ src/me/BadutCrew/GarfieldPlugin/Mob/Garfield.java
@@ -4,12 +4,14 @@ import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.craftbukkit.v1_16_R3.CraftWorld;
 
-import net.minecraft.server.v1_16_R3.EntityPlayer;
 import net.minecraft.server.v1_16_R3.ChatComponentText;
+import net.minecraft.server.v1_16_R3.EntityPlayer;
 import net.minecraft.server.v1_16_R3.EntityRavager;
 import net.minecraft.server.v1_16_R3.EntityTypes;
+import net.minecraft.server.v1_16_R3.PathfinderGoalLeapAtTarget;
 import net.minecraft.server.v1_16_R3.PathfinderGoalMeleeAttack;
 import net.minecraft.server.v1_16_R3.PathfinderGoalNearestAttackableTarget;
+import net.minecraft.server.v1_16_R3.PathfinderGoalRandomStroll;
 
 public class Garfield extends EntityRavager{
 	
@@ -21,7 +23,7 @@ public class Garfield extends EntityRavager{
         this.setHealth(400.0F);
         
         this.setCustomName(new ChatComponentText(ChatColor.GOLD + "" + ChatColor.BOLD + "Garfield"));
-        this.setCustomNameVisible(true);      
+        this.setCustomNameVisible(true);
     }
     
     @SuppressWarnings("unchecked")
@@ -29,7 +31,8 @@ public class Garfield extends EntityRavager{
     public void initPathfinder() {
     	
     	this.targetSelector.a(0, new PathfinderGoalNearestAttackableTarget<EntityPlayer>(this, EntityPlayer.class, false));
-    	this.goalSelector.a(1, new PathfinderGoalMeleeAttack(this, 1.0F, false));
-    	
+    	this.goalSelector.a(1, new PathfinderGoalLeapAtTarget(this, 0.6F));
+    	this.goalSelector.a(2, new PathfinderGoalMeleeAttack(this, 1.2F, false));
+    	this.goalSelector.a(3, new PathfinderGoalRandomStroll(this, 1.0F));
     }
 }
