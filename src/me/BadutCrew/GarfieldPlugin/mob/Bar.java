77a834b6c3a0eb796f6498bc83632a3ed72f590c
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2020-11-29 01:53:59 +0000

    Added mob text-speech

--- src/me/BadutCrew/GarfieldPlugin/mob/Bar.java
+++ src/me/BadutCrew/GarfieldPlugin/mob/Bar.java
@@ -1,8 +1,11 @@
 package me.BadutCrew.GarfieldPlugin.mob;
 
+import java.util.Random;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarFlag;
 import org.bukkit.boss.BarStyle;
 import org.bukkit.boss.BossBar;
 import org.bukkit.entity.Ravager;
@@ -21,6 +24,11 @@ public class Bar implements Listener {
 		new ItemStack(Material.BONE, 10),
 		new ItemStack(Material.ENDER_EYE, 2)
 	};
+	
+	private String[] catchPhrases = {
+			format("&6Stop it &lJON"), format("&6FEED ME!"), format("&6This is why I hate &lMondays"), 
+			format("&6Eat your heart out, &kYOU-MORTAL"), format("&6Stop it &lJON")
+	};
 
 	private double Health = 1;
 	public BossBar bossbar;
@@ -32,7 +40,7 @@ public class Bar implements Listener {
 		if(!event.getItem().getItemMeta().getDisplayName().contains("Lasagne"))
 			return;
 		
-        bossbar = Bukkit.createBossBar("Welcome!", BarColor.PINK, BarStyle.SOLID);
+        bossbar = Bukkit.createBossBar(format("&c&lGarfield the &kAlmighty"), BarColor.RED, BarStyle.SEGMENTED_10, BarFlag.CREATE_FOG);
         // Reset Health Value
         Health = 1;
         bossbar.setProgress(Health);
@@ -61,6 +69,9 @@ public class Bar implements Listener {
 		else {
 			Health = Health - (event.getDamage()/100);
 			bossbar.setProgress(Health);
+			
+			Random val = new Random();
+			Bukkit.broadcastMessage(catchPhrases[val.nextInt(catchPhrases.length)]);
 		}
 
 	}
@@ -82,11 +93,18 @@ public class Bar implements Listener {
 		event.setDroppedExp(400);
 		bossbar.removeAll();
 		
+		Bukkit.broadcastMessage(format("&6I'll be &l&4BACK &r&6for you ..."));
+		
 		ItemMeta meta = cursedItems[0].getItemMeta();
 		meta.setDisplayName(ChatColor.RED + "Jon's Bones");
 		cursedItems[0].setItemMeta(meta);
 		event.getEntity().getWorld().dropItemNaturally(event.getEntity().getLocation(), cursedItems[0]);
 		event.getEntity().getWorld().dropItemNaturally(event.getEntity().getLocation(), cursedItems[1]);
 		
+	}
+	
+	// String Format
+	private String format(String msg) {
+		return ChatColor.translateAlternateColorCodes('&', msg);
 	}
 }
