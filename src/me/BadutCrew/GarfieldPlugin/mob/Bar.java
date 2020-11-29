c44830c4deea6f7f49cd43aee88cab0cdce391db
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2020-11-29 01:17:45 +0000

    Update 4.0 - Bossbar
    Resolved error resulting from health entering negative bounds

--- src/me/BadutCrew/GarfieldPlugin/mob/Bar.java
+++ src/me/BadutCrew/GarfieldPlugin/mob/Bar.java
@@ -5,15 +5,23 @@ import org.bukkit.Material;
 import org.bukkit.boss.BarColor;
 import org.bukkit.boss.BarStyle;
 import org.bukkit.boss.BossBar;
-import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Ravager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
+import net.md_5.bungee.api.ChatColor;
 
 public class Bar implements Listener {
+	private ItemStack[] cursedItems = {
+		new ItemStack(Material.BONE, 10),
+		new ItemStack(Material.ENDER_EYE, 2)
+	};
+
 	private double Health = 1;
 	public BossBar bossbar;
 	
@@ -34,21 +42,51 @@ public class Bar implements Listener {
 	
 	@EventHandler
 	public void onHit(EntityDamageEvent event) {
-		if(!event.getEntityType().equals(EntityType.RAVAGER)) {
+		if(!(event.getEntity() instanceof Ravager)) {
+			return;
+		}
+		if(event.getEntity().getCustomName() == null) {
 			return;
 		}
 		
-		Health = Health - (event.getDamage()/100);
-		bossbar.setProgress(Health);
+		if(!event.getEntity().getCustomName().contains("Garfield")) {
+			return;
+		}
+		
+		// Prevent values subtracting into negatives
+		if((event.getDamage()/100) > Health) {
+			Health = Health - Health;
+		}
+		
+		else {
+			Health = Health - (event.getDamage()/100);
+			bossbar.setProgress(Health);
+		}
+
 	}
 	
 	@EventHandler
 	public void onDeath(EntityDeathEvent event) {
-		if(!event.getEntityType().equals(EntityType.RAVAGER)) {
+		if(!(event.getEntity() instanceof Ravager)) {
+			return;
+		}
+		if(event.getEntity().getCustomName() == null) {
 			return;
 		}
 		
+		if(!event.getEntity().getCustomName().contains("Garfield")) {
+			return;
+		}
+		
+		// Drop some loot and kill that boss bar
 		event.setDroppedExp(400);
 		bossbar.removeAll();
+		
+		ItemMeta meta = cursedItems[0].getItemMeta();
+		meta.setDisplayName(ChatColor.RED + "Jon's Bones");
+		cursedItems[0].setItemMeta(meta);
+		event.getEntity().getWorld().dropItemNaturally(event.getEntity().getLocation(), cursedItems[0]);
+		event.getEntity().getWorld().dropItemNaturally(event.getEntity().getLocation(), cursedItems[1]);
+		
 	}
 }
